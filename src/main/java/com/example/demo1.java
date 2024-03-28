b830cfa97b747552d1c579733364b19d8a58b5ae
lhq <liuhangqillll@163.com>
2024-03-28 20:21:29 +0800

    master commit third

--- src/main/java/com/example/demo1.java
+++ src/main/java/com/example/demo1.java
@@ -6,6 +6,8 @@ public class demo1 {
         System.out.println("2");
         System.out.println("3");
         System.out.println("4");
+        System.out.println("hot-fix");
+        System.out.println("master!!");
 
     }
 }
